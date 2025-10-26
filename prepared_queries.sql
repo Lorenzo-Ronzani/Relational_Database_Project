adbe1ec7c56d6ae2615721a6fcfbd3653841bb14
wlomazzi <wlomazzi@gmail.com>
2025-10-26 13:12:11 -0600

    Update queries

---
+++ prepared_queries.sql
@@ -0,0 +1,24 @@
+/*================================================================================================
+  DATA2201 – Phase 1
+  File: PreparedQueries.sql
+  Group D: Wesley Lomazzi, Lorenzo Ranzoni, Gabriel Passarelli
+  Description: Bank Database Prepared Queries
+  SKS National Bank
+ =================================================================================================
+  ERD
+  https://dbdiagram.io/d/BankDatabase-Diagram-68e56472d2b621e422b9b2cc
+  https://dbdocs.io/wlomazzi/Bank-Database?view=relationships
+  https://github.com/Lorenzo-Ronzani/Relational_Database_Project
+
+  Prepared Queries 
+  Prepare relevant queries for your populated database. Create an SQL file named 
+  “prepared_queries.sql”. 
+  This file should contain 10 queries that meet the following requirements: 
+  # Each query is in a stored procedure or user-defined function format. 
+  # Each query performs a meaningful action based on the case study. 
+  # Each query includes a comment that describes the purpose of the query. 
+  # Each query has a separate SQL statement that tests the query.
+
+*/
+
+
---
+++ wlomazzi_prepared_queries.sql
@@ -0,0 +1,23 @@
+/*================================================================================================
+  DATA2201 – Phase 1
+  File: wlomazzi-queries.sql
+  Group D: Wesley Lomazzi, Lorenzo Ranzoni, Gabriel Passarelli
+  Description: Bank Database queries by Wesley Lomazzi
+  SKS National Bank
+ =================================================================================================
+  ERD
+  https://dbdiagram.io/d/BankDatabase-Diagram-68e56472d2b621e422b9b2cc
+  https://dbdocs.io/wlomazzi/Bank-Database?view=relationships
+  https://github.com/Lorenzo-Ronzani/Relational_Database_Project
+
+  Prepared Queries 
+  Prepare relevant queries for your populated database. Create an SQL file named 
+  “prepared_queries.sql”. 
+  This file should contain at least 3 queries that meet the following requirements: 
+  # Each query is in a stored procedure or user-defined function format. 
+  # Each query performs a meaningful action based on the case study. 
+  # Each query includes a comment that describes the purpose of the query. 
+  # Each query has a separate SQL statement that tests the query.
+
+*/
+
